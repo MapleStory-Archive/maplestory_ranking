4bf52070e677cbadd7a3977466c67fa881f39845
invsc <invsc@naver.com>
2021-06-10 22:25:23 +0900

    캐릭터 직업 변수명('job'->'class' 변경)

--- html_to_json.py
+++ html_to_json.py
@@ -1,10 +1,13 @@
 import json
-from bs4.dammit import EncodingDetector
 import requests
 from bs4 import BeautifulSoup as bs
 
+#TODO: 전체 반복문 추가, JSON파일 하나당 캐릭터 몇개 추가?, MIN_LEVEL까지 반복 조건
+#TODO: 반복문 조건을 어떻게.. 페이지 단위(for).. 레벨단위(while)... 어떻게...??
+
 # maple.gg에서 파일 뽑은후 json으로 저장
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36'}
+
 # request
 html = requests.get(f'https://maple.gg/rank/total?page={1}', headers=headers)
 # bs
@@ -22,8 +25,8 @@ for character in char_list:
     data[rank_number]['server'] = character.select_one('td.align-middle > div.d-inline-block > img')['alt'] # server
     data[rank_number]['name'] = character.select_one('td.align-middle > div.d-inline-block > a > img')['alt'] # name
     data[rank_number]['level'] = character.select_one('td:nth-child(2) > div:nth-child(2) > div.font-size-0 > span:nth-child(1)').string[-3:] # level
-    data[rank_number]['job'] = character.select_one('td:nth-child(2) > div:nth-child(2) > div.font-size-0 > span:nth-child(3)').string # job
-    data[rank_number]['popularity'] = character.select_one('td.align-middle.d-none.d-sm-table-cell.text-center').string # popularity
+    data[rank_number]['class'] = character.select_one('td:nth-child(2) > div:nth-child(2) > div.font-size-0 > span:nth-child(3)').string # class
+    data[rank_number]['popularity'] = ''.join(character.select_one('td.align-middle.d-none.d-sm-table-cell.text-center').string.split(',')) # popularity
     # guild : 길드마크 있음, 길드마크 없음, 길드가 없음
     try:
         data[rank_number]['guild'] = character.select_one('td:nth-child(4) > a > img')['alt'] # 길드마크 있음
@@ -38,6 +41,6 @@ for character in char_list:
 
 
 # file_wirte
-# 1페이지(20캐릭터)
+# 20캐릭터(1페이지)*10페이지
 with open('test.json', 'w', encoding='utf-8') as data_file:
     json.dump(data, data_file, ensure_ascii=False, indent=4)
